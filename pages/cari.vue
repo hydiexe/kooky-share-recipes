b6a720719cbf40dc9bf62e11e298219f2feb36fc
hydiexe <nexuszeoviennazabrizkie@gmail.com>
2023-09-09 20:22:11 +0700

    fixing search feature

--- pages/cari.vue
+++ pages/cari.vue
@@ -1,54 +1,56 @@
 <template>
     <div class="py-4">
-        <div class="container">
-            <div class="title border-bottom">
-                <h5>Search Recipes</h5>
-            </div>
-            <div class="search-bar">
-                <input type="text" placeholder="Search...">
-                <button>Search</button>
-            </div>
-            <div class="list-task">
-                <!-- Loop through filtered recipes (based on searchQuery) -->
-                <div v-for="recipe in filteredRecipes" :key="recipe.id" class="item-task d-flex align-items-center border-bottom pt-3 pb-4">
-                    <div class="title-task">{{ recipe.title }}</div>
-                </div>
-            </div>
+      <div class="container">
+        <div class="title border-bottom">
+          <h5>Cari Resep</h5>
         </div>
+        <div class="search-bar">
+          <input v-model="searchQuery" type="text" placeholder="Cari..." @input="performSearch">
+          <button @click="performSearch">Cari</button>
+        </div>
+        <div class="list-task">
+          <!-- Melooping resep yang telah difilter (berdasarkan searchQuery) -->
+          <div v-for="recipe in filteredRecipes" :key="recipe.id" class="item-task d-flex align-items-center border-bottom pt-3 pb-4">
+            <div class="title-task">{{ recipe.title }}</div>
+          </div>
+        </div>
+      </div>
     </div>
-</template>
+  </template>
   
-<script>
-export default {
-    name: 'Cari',
+  <script>
+  export default {
     data() {
-    return {
-        searchQuery: '', // Store user's search query
-        recipes: [  // Initialize the recipes array
-            { id: 1, title: 'Recipe 1', ingredients: '...', instructions: '...', category: 'makanan' },
-            { id: 2, title: 'Recipe 2', ingredients: '...', instructions: '...', category: 'minuman' },
-            // Add more recipes as needed
+      return {
+        searchQuery: '', // Menyimpan kata kunci pencarian pengguna
+        recipes: [
+          // Data resep Anda di sini
         ],
-        };
+      };
     },
     computed: {
-        filteredRecipes() {
-        // Use this.searchQuery to filter recipes based on user's input
+      filteredRecipes() {
         const query = this.searchQuery.toLowerCase();
         return this.recipes.filter(recipe => {
-            return recipe.title.toLowerCase().includes(query) ||
-                recipe.ingredients.toLowerCase().includes(query) ||
-                recipe.instructions.toLowerCase().includes(query);
+          return (
+            recipe.title.toLowerCase().includes(query) ||
+            recipe.ingredients.toLowerCase().includes(query) ||
+            recipe.instructions.toLowerCase().includes(query)
+          );
         });
-        },
+      },
     },
     methods: {
-        performSearch() {
-        // This method will be called when the user clicks the "Search" button
-        // You can also call it when the search query changes (e.g., when typing)
-        // Implement any additional logic you need for searching
-        },
+      performSearch() {
+        // Metode ini akan dipanggil saat pengguna mengklik tombol "Cari"
+        // Anda juga dapat memanggilnya saat query pencarian berubah (misalnya, saat mengetik)
+        // Fungsionalitas pencarian diimplementasikan dalam computed property "filteredRecipes"
+      },
     },
-};
-</script>
+  };
+  </script>
+  
+  <style>
+  /* Tambahkan gaya khusus untuk komponen ini jika diperlukan */
+  </style>
   
