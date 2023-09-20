b4136a7b5883f1c721643a97fb894e14b2f51595
hydiexe <nexuszeoviennazabrizkie@gmail.com>
2023-09-20 09:55:05 +0700

    code syntax fixing

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,24 +1,24 @@
 <!-- eslint-disable vue/no-reserved-component-names -->
 <template>
     <div id="app">
-        <Header />
+        <HeaderPage />
 
         <Nuxt />
 
-        <Footer />
+        <FooterPage />
     </div>
 </template>
 
 <script>
 // Memanggil components
-import Header from "@/components/Header.vue"
-import Footer from "@/components/Footer.vue"
+import Header from "@/components/HeaderPage.vue"
+import Footer from "@/components/FooterPage.vue"
 
 export default {
     components: {
-        // eslint-disable-next-line vue/no-reserved-component-names
+        // eslint-disable-next-line vue/no-reserved-component-names, vue/no-unused-components
         Header,
-        // eslint-disable-next-line vue/no-reserved-component-names
+        // eslint-disable-next-line vue/no-reserved-component-names, vue/no-unused-components
         Footer
     }
 } 
